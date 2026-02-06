398258d3399a0457f998750e7ab01b8efee0d1ee
Matthew Zahra <matthewzahra04@gmail.com>
2026-02-06 16:49:07 +0000

    ensured that the spheres generated are always valid

--- RL/generate_action_sets.py
+++ RL/generate_action_sets.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 '''
 Here we write functions ot help us compute F(x,a), where x is a concrete state and a an abstract action (or similar?)
 '''
@@ -5,7 +7,8 @@ Here we write functions ot help us compute F(x,a), where x is a concrete state a
 # TODO - make this more modular so that we can better implement interface functions that can be easily swapped in and out 
 
 # the most basic one is simply an L_infinity ball around a with a given direction - this forms a hyperrectangle
-def L_infinity(centre,distances):
+# we must clip it so that if we are on the extreme of the action space, we don't allow invalid actions (if lower and upper bounds provided)
+def L_infinity(centre, distances, lower_bounds=None, upper_bounds=None):
 	'''
 	:param centre: centre coordinates of the ball
 	:param distance: radius for each dimension - it does not need to be the same everywhere. Assumes all values are non-negative!!!
@@ -14,4 +17,7 @@ def L_infinity(centre,distances):
 	'''
 
 	assert centre.size == distances.size
-	return centre-distances,centre+distances # TODO - check this return type - should it be in a list? 
+	if lower_bounds is not None or upper_bounds is not None:
+		return np.maximum(centre-distances,lower_bounds),np.minimum(centre+distances, upper_bounds) # TODO - check this return type - should it be in a list? 
+	else:
+		return centre-distances, centre+distances
