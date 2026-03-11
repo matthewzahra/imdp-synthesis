fd31fc1652ab4caf6dd4aa651960f1e6357d16d3
Matthew Zahra <matthewzahra04@gmail.com>
2026-03-11 17:33:12 +0000

    made goal region larger!

--- benchmarks/Dubins_small.py
+++ benchmarks/Dubins_small.py
@@ -51,7 +51,7 @@ class Dubins_small(DubinsSmallDynamics):
         # ], dtype=float)
 
         self.goal = np.array([
-            [[8, -10, -np.pi], [10, 10, np.pi]]
+            [[7, -10, -np.pi], [10, 10, np.pi]]
         ], dtype=float)
 
         # self.critical = np.array([
@@ -61,7 +61,7 @@ class Dubins_small(DubinsSmallDynamics):
 
         self.critical = np.array([
             # [[-1, 9, -np.pi], [1, 10, np.pi]],
-            [[-1, -3, -np.pi], [1, 3, np.pi]],
+            [[-1, -4, -np.pi], [1, 5, np.pi]],
             # [[-6, -1, -np.pi], [-1, 2, np.pi]],
             # [[-1, -10, -np.pi], [1, -9, np.pi]]
         ], dtype=float)
